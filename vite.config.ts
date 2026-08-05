b390b11d2017a7bb7d6d6b4a22bccd21edb388a9
charlespura <charles051902pura@gmail.com>
2026-08-05 16:41:22 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FocusForge/', // Replace 'FocusForge' with your repository name
   server: {
     port: 3000,
   },
-  css: {
-    postcss: './postcss.config.js',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
-})
+});
